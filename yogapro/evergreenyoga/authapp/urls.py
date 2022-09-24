c357c7b115a426948b5ed049c661f2016ac76994
adiba45 <adibabanu45@gmail.com>
2022-09-24 22:52:07 +0530

    Initial commit

---
+++ yogapro/evergreenyoga/authapp/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from authapp import views
+
+urlpatterns = [
+    path('',views.Home,name="Home"),
+    path('about',views.About,name="about"),
+    path('asanas',views.Asanas,name="asanas"),
+    path('signup',views.signup,name="signup"),
+    path('login',views.handlelogin,name="handlelogin"),
+    path('logout',views.handleLogout,name="handleLogout"),
+    path('contact',views.contact,name="contact"),
+    path('join',views.enroll,name="enroll"),
+    path('profile',views.profile,name="profile"),
+    path('gallery',views.gallery,name="gallery"),
+    path('attendance',views.attendance,name="attendance"),
+
+    
+]
